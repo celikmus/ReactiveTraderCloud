b45b64e0cf284ff29848e665c738a68507af5bc5
Mustafa Celik <celikmus@gmail.com>
2017-10-11 16:50:02 +0100

    Extracted dictionary creation in separate file

--- src/client/src/services/blotterService.ts
+++ src/client/src/services/blotterService.ts
@@ -3,11 +3,9 @@ import { TradeMapper } from './mappers'
 import { logger, RetryPolicy } from '../system'
 import '../system/observableExtensions/retryPolicyExt'
 import { ServiceConst } from '../types'
-import { BehaviorSubject } from 'rxjs/BehaviorSubject'
-import LastValueObservableDictionary from '../system/service/lastValueObservableDictionary'
 import { ServiceStatus } from '../types/serviceStatus'
 import * as _ from 'lodash'
-import { ConnectionStatus } from '../types/connectionStatus'
+import createMulticastDictionaryStream from '../system/service/createMulticastDictionaryStream'
 
 const log = logger.create('BlotterService')
 
@@ -26,74 +24,12 @@ function createServiceStatus(cache, serviceType): ServiceStatus {
 }
 
 export default function blotterService(connection, referenceDataService) {
-  const HEARTBEAT_TIMEOUT = 3000
   const serviceType = ServiceConst.BlotterServiceKey
-  const connectionStatus = connection.connectionStatusStream
-    .map(status => status === ConnectionStatus.connected)
-    .publish()
-    .refCount()
-  const errorOnDisconnectStream = connectionStatus
-    .filter(isConnected => !isConnected)
-    .take(1)
-    .flatMap(() => Observable.throw('Underlying connection disconnected'))
-  const serviceInstanceDictionaryObservable = connection
-    .subscribeToTopic('status')
-    .filter(s => s.Type === serviceType)
-    .map(status => ({
-      serviceType: status.Type,
-      serviceId: status.Instance,
-      timestamp: status.Timestamp,
-      serviceLoad: status.Load,
-      isConnected: true
-    }))
-    // If the underlying connection goes down we error the stream.
-    // Do this before the grouping so all grouped streams error.
-    .merge(errorOnDisconnectStream)
-    // .filter(item => item) // if item is false don't continue
-    .groupBy(serviceStatus => serviceStatus.serviceId)
-    // add service instance level heartbeat timeouts, i.e. each service instance can disconnect independently
-    .debounceOnMissedHeartbeat(
-      HEARTBEAT_TIMEOUT,
-      serviceId => ({
-        serviceType,
-        serviceId,
-        timestamp: NaN,
-        serviceLoad: NaN,
-        isConnected: false
-      }),
-      Scheduler.async
-    )
-    // create a hash of properties which represent significant change in a status, we'll use this to filter out duplicates
-    .distinctUntilChangedGroup(
-      (status, statusNew) =>
-        status.isConnected === statusNew.isConnected &&
-        status.serviceLoad === statusNew.serviceLoad
-    )
-    // flattens all our service instances stream into an observable dictionary so we query the service with the least load on a per-subscribe basis
-    .toServiceStatusObservableDictionary(
-      serviceStatus => serviceStatus.serviceId
-    )
-    // catch the disconnect error of the outer stream and continue with an empty (thus disconnected) dictionary
-    .catch(() => Observable.of(new LastValueObservableDictionary()))
-
-
-  const connectedServiceInstanceObservable = connectionStatus.filter(isConnected => isConnected)
-    .take(1)
-    // flatMap: since we're just taking one, this effectively just continues the stream by subscribing to serviceInstanceDictionaryStream
-    .mergeMap(() => serviceInstanceDictionaryObservable)
-    // repeat after disconnects
-    .repeat()
-
-  const multicastServiceInstanceDictionaryStream = connectedServiceInstanceObservable.multicast(
-    new BehaviorSubject(new LastValueObservableDictionary())
-  )
-  const serviceStatusStream = multicastServiceInstanceDictionaryStream
-    .map(cache => createServiceStatus(cache, serviceType))
-    .publish()
-    .refCount()
-
+  const multicastServiceInstanceDictionaryStream = createMulticastDictionaryStream(connection, serviceType)
   return {
-    serviceStatusStream,
+    serviceStatusStream: multicastServiceInstanceDictionaryStream
+      .map(cache => createServiceStatus(cache, serviceType))
+      .share(),
     getTradesStream() {
       const streamOperation = Observable.create(o => {
         log.debug('Subscribing to trade stream')
@@ -131,7 +67,6 @@ export default function blotterService(connection, referenceDataService) {
             err => o.error(err),
             () => o.complete()
           )
-
 
         multicastServiceInstanceDictionaryStream.getServiceWithMinLoad().subscribe(
           serviceInstanceStatus => {
---
+++ src/client/src/system/service/createMulticastDictionaryStream.ts
@@ -0,0 +1,69 @@
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+import LastValueObservableDictionary from './lastValueObservableDictionary'
+import { Observable, Scheduler } from 'rxjs'
+import { HEARTBEAT_TIMEOUT } from '../../serviceConfigs'
+import { ConnectionStatus } from '../../types/connectionStatus'
+
+export default function createMulticastDictionaryStream(connection, serviceType) {
+
+  const connectionStatus = connection.connectionStatusStream
+    .map(status => status === ConnectionStatus.connected)
+    .share()
+
+  const errorOnDisconnectStream = connectionStatus
+    .filter(isConnected => !isConnected)
+    .take(1)
+    .flatMap(() => Observable.throw('Underlying connection disconnected'))
+
+  const serviceInstanceDictionaryObservable = connection
+    .subscribeToTopic('status')
+    .filter(s => s.Type === serviceType)
+    .map(status => ({
+      serviceType: status.Type,
+      serviceId: status.Instance,
+      timestamp: status.Timestamp,
+      serviceLoad: status.Load,
+      isConnected: true
+    }))
+    // If the underlying connection goes down we error the stream.
+    // Do this before the grouping so all grouped streams error.
+    .merge(errorOnDisconnectStream)
+    // .filter(item => item) // if item is false don't continue
+    .groupBy(serviceStatus => serviceStatus.serviceId)
+    // add service instance level heartbeat timeouts, i.e. each service instance can disconnect independently
+    .debounceOnMissedHeartbeat(
+      HEARTBEAT_TIMEOUT,
+      serviceId => ({
+        serviceType,
+        serviceId,
+        timestamp: NaN,
+        serviceLoad: NaN,
+        isConnected: false
+      }),
+      Scheduler.async
+    )
+    // create a hash of properties which represent significant change in a status, we'll use this to filter out duplicates
+    .distinctUntilChangedGroup(
+      (status, statusNew) =>
+        status.isConnected === statusNew.isConnected &&
+        status.serviceLoad === statusNew.serviceLoad
+    )
+    // flattens all our service instances stream into an observable dictionary so we query the service with the least load on a per-subscribe basis
+    .toServiceStatusObservableDictionary(
+      serviceStatus => serviceStatus.serviceId
+    )
+    // catch the disconnect error of the outer stream and continue with an empty (thus disconnected) dictionary
+    .catch(() => Observable.of(new LastValueObservableDictionary()))
+
+  return connectionStatus
+    .filter(isConnected => isConnected)
+    .take(1)
+    // flatMap: since we're just taking one, this effectively just continues the stream by subscribing to serviceInstanceDictionaryStream
+    .mergeMap(() => serviceInstanceDictionaryObservable)
+    // repeat after disconnects
+    .repeat()
+    .multicast(
+      new BehaviorSubject(new LastValueObservableDictionary())
+    )
+}
+
